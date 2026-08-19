7cf64302736e51341558b5d5253b6a7e44723395
UmsrAkne <sibata486@gmail.com>
2026-08-20 00:17:14 +0900

    chore: Update default application title
    - Change default title from "Wpf Application" to "WpfProjectTemplate"

--- WpfProjectTemplate/ViewModels/MainWindowViewModel.cs
+++ WpfProjectTemplate/ViewModels/MainWindowViewModel.cs
@@ -5,7 +5,7 @@ namespace WpfProjectTemplate.ViewModels
     // ReSharper disable once ClassNeverInstantiated.Global
     public class MainWindowViewModel : BindableBase
     {
-        private string title = "Wpf Application";
+        private string title = "WpfProjectTemplate";
 
         public string Title { get => title; set => SetProperty(ref title, value); }
     }
