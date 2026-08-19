b0bc8c1315e4f9bb70bfcafb655bffaefeba3b49
UmsrAkne <sibata486@gmail.com>
2026-08-19 16:24:35 +0900

    init

---
+++ WpfProjectTemplate/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using Prism.DryIoc;
+using Prism.Ioc;
+using WpfProjectTemplate.Views;
+
+namespace WpfProjectTemplate;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : PrismApplication
+{
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+    }
+
+    protected override Window CreateShell()
+    {
+        return Container.Resolve<MainWindow>();
+    }
+}
---
+++ WpfProjectTemplate/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Prism.Mvvm;
+
+namespace WpfProjectTemplate.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class MainWindowViewModel : BindableBase
+    {
+        private string title = "Wpf Application";
+
+        public string Title { get => title; set => SetProperty(ref title, value); }
+    }
+}
