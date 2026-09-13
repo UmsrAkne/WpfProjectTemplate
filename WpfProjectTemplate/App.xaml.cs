d852a7a5a611f526ae8768e89fdac30b86bd1e8e
UmsrAkne <sibata486@gmail.com>
2026-09-13 22:09:55 +0900

    chore: Remove unused `PrismApplication` inheritance in `App` class

--- WpfProjectTemplate/App.xaml.cs
+++ WpfProjectTemplate/App.xaml.cs
@@ -8,7 +8,7 @@ namespace WpfProjectTemplate;
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
-public partial class App : PrismApplication
+public partial class App
 {
     protected override void RegisterTypes(IContainerRegistry containerRegistry)
     {
